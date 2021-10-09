809cad9b1c7508523a7863f1201e606c670fe370
StevenVallarsa <orionsjv@gmail.com>
2021-10-09 18:49:18 -0400

    React Router and Breadcrumbs

--- src/shared/campsites.js
+++ src/shared/campsites.js
@@ -1,180 +1,34 @@
 export const CAMPSITES = [
-  {
-    id: 0,
-    name: "React Lake Campground",
-    image: "assets/images/react-lake.jpg",
-    elevation: 1233,
-    description:
-      "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers.",
-    comments: [
-      {
+    {
         id: 0,
-        rating: 5,
-        text: "0 What a magnificent view!",
-        author: "Tinus Lorvaldes",
-        date: "2018-10-25T16:30Z",
-      },
-      {
+        name: "React Lake Campground",
+        image: "/assets/images/react-lake.jpg",
+        elevation: 1233,
+        featured: false,
+        description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
+    },
+    {
         id: 1,
-        rating: 4,
-        text: "0 The campground was beautiful, but the bugs could bite sometimes.",
-        author: "Brennen Ech",
-        date: "2017-06-17T03:33Z",
-      },
-      {
+        name:"Chrome River Campground ",
+        image: "/assets/images/chrome-river.jpg",
+        elevation: 877,
+        featured: false,
+        description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
+    },
+    {
         id: 2,
-        rating: 5,
-        text: "0 I caught a world-record-breaking trout here on my last visit.",
-        author: "McKenzie Sebastian",
-        date: "2019-02-18T18:12Z",
-      },
-      {
+        name:"Breadcrumb Trail Campground",
+        image: "/assets/images/breadcrumb-trail.jpg",
+        elevation: 2901,
+        featured: false,
+        description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
+    },
+    {
         id: 3,
-        rating: 5,
-        text: "0 A wonderful place to reconnect with nature.",
-        author: "Jordan Runn",
-        date: "2019-08-04T20:11Z",
-      },
-      {
-        id: 4,
-        rating: 4,
-        text: "0 The stars at night were magnificent!",
-        author: "Ann Dabramov",
-        date: "2018-07-23T19:44Z",
-      },
-    ],
-  },
-  {
-    id: 1,
-    name: "Chrome River Campground ",
-    image: "assets/images/chrome-river.jpg",
-    elevation: 877,
-    description:
-      "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River.",
-    comments: [
-      {
-        id: 0,
-        rating: 5,
-        text: "1 What a magnificent view!",
-        author: "Tinus Lorvaldes",
-        date: "2018-10-25T16:30Z",
-      },
-      {
-        id: 1,
-        rating: 4,
-        text: "1 The campground was beautiful, but the bugs could bite sometimes.",
-        author: "Brennen Ech",
-        date: "2017-06-17T03:33Z",
-      },
-      {
-        id: 2,
-        rating: 5,
-        text: "1 I caught a world-record-breaking trout here on my last visit.",
-        author: "McKenzie Sebastian",
-        date: "2019-02-18T18:12Z",
-      },
-      {
-        id: 3,
-        rating: 5,
-        text: "1 A wonderful place to reconnect with nature.",
-        author: "Jordan Runn",
-        date: "2019-08-04T20:11Z",
-      },
-      {
-        id: 4,
-        rating: 4,
-        text: "1 The stars at night were magnificent!",
-        author: "Ann Dabramov",
-        date: "2018-07-23T19:44Z",
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: "Breadcrumb Trail Campground",
-    image: "assets/images/breadcrumb-trail.jpg",
-    elevation: 2901,
-    description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground.",
-    comments: [
-      {
-        id: 0,
-        rating: 5,
-        text: "2 What a magnificent view!",
-        author: "Tinus Lorvaldes",
-        date: "2018-10-25T16:30Z",
-      },
-      {
-        id: 1,
-        rating: 4,
-        text: "2 The campground was beautiful, but the bugs could bite sometimes.",
-        author: "Brennen Ech",
-        date: "2017-06-17T03:33Z",
-      },
-      {
-        id: 2,
-        rating: 5,
-        text: "2 I caught a world-record-breaking trout here on my last visit.",
-        author: "McKenzie Sebastian",
-        date: "2019-02-18T18:12Z",
-      },
-      {
-        id: 3,
-        rating: 5,
-        text: "2 A wonderful place to reconnect with nature.",
-        author: "Jordan Runn",
-        date: "2019-08-04T20:11Z",
-      },
-      {
-        id: 4,
-        rating: 4,
-        text: "2 The stars at night were magnificent!",
-        author: "Ann Dabramov",
-        date: "2018-07-23T19:44Z",
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: "Redux Woods Campground",
-    image: "assets/images/redux-woods.jpg",
-    elevation: 42,
-    description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
-    comments: [
-      {
-        id: 0,
-        rating: 5,
-        text: "3 What a magnificent view!",
-        author: "Tinus Lorvaldes",
-        date: "2018-10-25T16:30Z",
-      },
-      {
-        id: 1,
-        rating: 4,
-        text: "3 The campground was beautiful, but the bugs could bite sometimes.",
-        author: "Brennen Ech",
-        date: "2017-06-17T03:33Z",
-      },
-      {
-        id: 2,
-        rating: 5,
-        text: "3 I caught a world-record-breaking trout here on my last visit.",
-        author: "McKenzie Sebastian",
-        date: "2019-02-18T18:12Z",
-      },
-      {
-        id: 3,
-        rating: 5,
-        text: "3 A wonderful place to reconnect with nature.",
-        author: "Jordan Runn",
-        date: "2019-08-04T20:11Z",
-      },
-      {
-        id: 4,
-        rating: 4,
-        text: "3 The stars at night were magnificent!",
-        author: "Ann Dabramov",
-        date: "2018-07-23T19:44Z",
-      },
-    ],
-  },
+        name:"Redux Woods Campground",
+        image: "/assets/images/redux-woods.jpg",
+        elevation: 42,
+        featured: true,
+        description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
+    }
 ];
